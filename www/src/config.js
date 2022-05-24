71255addf5b93d8617bf7b91ebf09090e71d1cd8
ms-takasaki <takasaki@ulvaniac.co.jp>
2022-05-24 22:40:02 +0900

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "5ucn4lsss9glsfgr8sfetv1q43",     // CognitoClientID
-  "api_base_url": "https://rt2euh0w4d.execute-api.ap-northeast-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "aws_user_pools_web_client_id": "2m2nblisj4pv9vdvor1a800714",     // CognitoClientID
+  "api_base_url": "https://unyqji7vk4.execute-api.ap-northeast-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-lambda-refarch-webapp-1.auth.ap-northeast-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d1byrl9lria9lg.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://master.d1uwc24ziuog2q.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
